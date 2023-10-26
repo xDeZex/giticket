66be5fc792e02b2fde84757bb96d4ce0ae3494f3
Oliver Lillberg <44681414+xDeZex@users.noreply.github.com>
2023-10-26 14:23:31 +0200

    reopens issue

--- giticket/giticket.py
+++ giticket/giticket.py
@@ -35,7 +35,7 @@ def update_commit_message(filename, regex, mode, format_string):
                 tickets = [branch.split(six.text_type('_'))[0]]
             tickets = [t.strip() for t in tickets]
             
-            tickets = [re.sub("^[0-9]*-", "", ticket) for ticket in tickets]
+            tickets = [re.sub("^[0-9]*-", "reopens \\1# ", ticket) for ticket in tickets]
 
             new_commit_msg = format_string.format(
                 ticket=tickets[0], tickets=', '.join(tickets),
